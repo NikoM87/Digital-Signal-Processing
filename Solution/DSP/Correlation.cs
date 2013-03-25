1858ac5df6475ba5954666052c938e463c0dd546
Nikolai <NikoM87@gmail.com>
2013-03-25 19:46:12 +0600

    Корреляция

---
+++ Solution/DSP/Correlation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace DSP
+{
+    public class Correlation
+    {
+        public double Execute( double[] x1, double[] x2)
+        {
+            int length = Math.Min( x1.Length, x2.Length );
+           
+            double r = 0;
+            for ( int i = 0; i < length; i++ )
+            {
+                r += x1[i] * x2[i];
+            }
+            r /= length;
+
+            return r;
+        }
+    }
+}
---
+++ Solution/Tests/CorrelationTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DSP;
+
+
+namespace Tests
+{
+    [TestClass]
+    public class CorrelationTest
+    {
+        private readonly Correlation _correlation;
+
+
+        public CorrelationTest()
+        {
+            _correlation = new Correlation();
+        }
+
+
+        [TestMethod]
+        public void TestCorrelation()
+        {
+            double[] signal1 = {4, 2, -1, 3, -2, -6, -5, 4, 5};
+            double[] signal2 = {-4, 1, 3, 7, 4, -2, -8, -2, 1};
+
+            double c = _correlation.Execute( signal1, signal2 );
+
+            Assert.AreEqual( 5, c );
+        }
+    }
+}
