eb55cddbcc47b74020d6376da1a582cc3c150a19
Nikolai <NikoM87@gmail.com>
2013-04-04 12:38:55 +0600

    Новый чанк для хранения массивов

---
+++ Solution/DSP/Infrastructure/Chanks/ChankArray.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+
+namespace DSP.Infrastructure.Chanks
+{
+    public class ChankArray : Chank
+    {
+        public const ushort ChankArrayType = 2;
+        private readonly List<Chank> _list;
+
+
+        public ChankArray( Stream chankStream )
+            : base( chankStream )
+        {
+            _list = new List<Chank>();
+
+            Id = ChankArrayType;
+        }
+
+
+        public int Length
+        {
+            get { return _list.Count; }
+        }
+
+
+
+        public void Add( Chank newChank )
+        {
+            _list.Add( newChank );
+        }
+    }
+}
---
+++ Solution/Tests/ChankArrayTest.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+
+using DSP.Infrastructure.Chanks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Tests
+{
+    [TestClass]
+    public class ChankArrayTest
+    {
+        private readonly ChankArray _array;
+
+
+        public ChankArrayTest()
+        {
+            Stream stream = new MemoryStream();
+            _array = new ChankArray( stream );
+        }
+
+
+        [TestMethod]
+        public void TestChankArrayCreate()
+        {
+            Assert.AreEqual( 2, _array.Id );
+            Assert.AreEqual( 0, _array.Length );
+        }
+
+
+        [TestMethod]
+        public void TestAddChank()
+        {
+            Chank newChank = new ChankArray( new MemoryStream() );
+
+            _array.Add( newChank );
+            _array.Add( newChank );
+
+            Assert.AreEqual( 2, _array.Length );
+        }
+    }
+}
